9ede709d1617223c8f226f6c3bda6d6b5db6e35e
peter <petertaklee3@mail.com>
2024-02-14 09:27:28 +0900

    게시물 목록 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,38 +1,72 @@
 package com.KoreaIT.java.AM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
   public static void main(String[] args) {
-    System.out.println("==프로그램 시작==");
+    System.out.println("== 프로그램 시작 ==");
     Scanner sc = new Scanner(System.in);
 
-    while(true) {
+    int lastArticleId = 0;
+    List<Article> articles = new ArrayList<>();
 
-      System.out.println("명령어) ");
-      String cmd = sc.nextLine();
+    while (true) {
+      System.out.print("명령어 ) ");
+      String cmd = sc.nextLine().trim();
 
       if (cmd.length() == 0) {
         System.out.println("명령어를 입력하세요.");
         continue;
       }
 
-
       if (cmd.equals("system exit")) {
         break;
-
       }
-      if (cmd.equals("article wirte")){
-        System.out.println("제목 : ");
-        System.out.println("내용 : ");
+
+      if (cmd.equals("article write")) {
+        int id = lastArticleId + 1;
+        System.out.print("제목 : ");
+        String title = sc.nextLine();
+        System.out.print("내용 : ");
         String body = sc.nextLine();
+
+        Article article = new Article(id, title, body);
+        articles.add(article);
+
+        System.out.printf("%d번 글이 생성되었습니다.\n", id);
+        lastArticleId = id;
+
+      } else if (cmd.equals("article list")) {
+        if (articles.size() == 0) {
+          System.out.println("게시글이 없습니다.");
+          continue;
+        } else {
+          System.out.println(" 번호 | 제목");
+          for (int i = articles.size() -1; i >=1; i--){
+            Article article = articles.get(i);
+            System.out.printf("   %d   |   %s   \n", article.id, article.title);
+          }
+        }
+      } else {
+        System.out.println("존재하지 않는 명령어입니다.");
       }
     }
 
     sc.close();
+    System.out.println("== 프로그램 종료 ==");
+  }
+}
 
-    System.out.println("== 프로그램 종료==");
+class Article {
+  int id;
+  String title;
+  String body;
 
+  public Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
   }
 }
